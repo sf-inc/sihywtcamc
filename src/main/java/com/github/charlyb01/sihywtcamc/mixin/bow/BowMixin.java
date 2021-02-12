5f06d39e01e94121566209538ef7d50e5010247e
Charly <charly5bollinger@gmail.com>
2021-02-12 23:35:23 +0100

    Balanced power enchant #3
    For both players and any mob entities

---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/bow/BowMixin.java
@@ -0,0 +1,27 @@
+package com.github.charlyb01.sihywtcamc.mixin.bow;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(BowItem.class)
+public class BowMixin {
+    @ModifyVariable(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/PersistentProjectileEntity;setDamage(D)V", shift = At.Shift.AFTER),
+            method = "onStoppedUsing")
+    private PersistentProjectileEntity changePowerDamageB(PersistentProjectileEntity projectileEntity, ItemStack stack, World world, LivingEntity user,
+                                   int remainingUseTicks) {
+        int level = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
+        if (level > 0) {
+            projectileEntity.setDamage(projectileEntity.getDamage() - (double)level * 0.5D - 0.5D);
+            projectileEntity.setDamage(projectileEntity.getDamage() + (double)level * 0.4D);
+        }
+        return projectileEntity;
+    }
+}
---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/bow/ProjectileMixin.java
@@ -0,0 +1,28 @@
+package com.github.charlyb01.sihywtcamc.mixin.bow;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PersistentProjectileEntity.class)
+public abstract class ProjectileMixin {
+    @Shadow public abstract double getDamage();
+
+    @Shadow public abstract void setDamage(double damage);
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/PersistentProjectileEntity;setDamage(D)V", shift = At.Shift.AFTER),
+            method = "applyEnchantmentEffects")
+    private void changePowerDamageP(LivingEntity entity, float damageModifier, CallbackInfo ci) {
+        int level = EnchantmentHelper.getEquipmentLevel(Enchantments.POWER, entity);
+        if (level > 0) {
+            this.setDamage(this.getDamage() - (double)level * 0.5D - 0.5D);
+            this.setDamage(this.getDamage() + (double)level * 0.4D);
+        }
+    }
+}
