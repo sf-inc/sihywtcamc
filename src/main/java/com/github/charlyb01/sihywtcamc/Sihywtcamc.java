49f089d8066bd8fc1592a9ecd4ae550c1f84139f
Charly <charly5bollinger@gmail.com>
2021-02-16 20:37:05 +0100

    Stackable potions (Fixed #6)
    Non-throwable potions are stackable by 16

--- src/main/java/com/github/charlyb01/sihywtcamc/Sihywtcamc.java
+++ src/main/java/com/github/charlyb01/sihywtcamc/Sihywtcamc.java
@@ -1,10 +1,12 @@
 package com.github.charlyb01.sihywtcamc;
 
+import com.github.charlyb01.sihywtcamc.mixin.ItemAccessor;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Items;
 
 public class Sihywtcamc implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        ((ItemAccessor) Items.POTION).setMaxCount(16);
     }
 }
---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/ItemAccessor.java
@@ -0,0 +1,13 @@
+package com.github.charlyb01.sihywtcamc.mixin;
+
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Item.class)
+public interface ItemAccessor {
+    @Accessor
+    @Mutable
+    void setMaxCount(int newCount);
+}
