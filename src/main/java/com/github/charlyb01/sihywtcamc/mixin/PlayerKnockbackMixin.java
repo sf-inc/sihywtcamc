805eb211631aa6eb755d520c4e8329c9602d26a7
Charly <charly5bollinger@gmail.com>
2021-02-19 21:24:19 +0100

    Projectile knockback #11
    Players get knocked back by eggs and snow balls even if they don't damage them. Also sounds and look like player hurt even if no damage. Can stop the knockback with shield -> Should work but need verification in server

---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/PlayerKnockbackMixin.java
@@ -0,0 +1,27 @@
+package com.github.charlyb01.sihywtcamc.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ShieldItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerKnockbackMixin extends LivingEntity {
+    @Shadow protected abstract void takeShieldHit(LivingEntity attacker);
+
+    protected PlayerKnockbackMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "damage", at = @At(value = "RETURN", ordinal = 3), cancellable = true)
+    private void takeAllKnockback(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(!(this.activeItemStack.getItem() instanceof ShieldItem) && super.damage(source, amount));
+    }
+}
