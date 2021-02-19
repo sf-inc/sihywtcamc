f8c9ef03211e6aef90d20871cccfc6f231f369db
Charly <charly5bollinger@gmail.com>
2021-02-19 21:41:54 +0100

    Projectile cooldown (Fixed #10)
    Snow balls and eggs have now a cooldown. 4 ticks seemed just like without cooldown (without causing lags) so I put to 5 ticks. If you have a shield, you can't spam (I mean holding the click) because it will shield because of the cooldown. So if you want to send a lot, you either need to take off your shield (and you can get knocked back) or you need to click at the right time

---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/projectile/EggMixin.java
@@ -0,0 +1,21 @@
+package com.github.charlyb01.sihywtcamc.mixin.projectile;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.EggItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EggItem.class)
+public class EggMixin {
+    @Inject(method = "use", at = @At("HEAD"))
+    private void addEggCooldown(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        user.getItemCooldownManager().set(Items.EGG, 5);
+    }
+}
---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/projectile/SnowBallMixin.java
@@ -0,0 +1,21 @@
+package com.github.charlyb01.sihywtcamc.mixin.projectile;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.SnowballItem;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SnowballItem.class)
+public class SnowBallMixin {
+    @Inject(method = "use", at = @At("HEAD"))
+    private void addSnowBallCooldown(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        user.getItemCooldownManager().set(Items.SNOWBALL, 5);
+    }
+}
