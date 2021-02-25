56952d43fdcd094e138e37ecdb8ff21ac5b18005
Charly <charly5bollinger@gmail.com>
2021-02-25 17:49:28 +0100

    Fix crash outside dev environment
    For mixins: using ordinal, not name!

--- src/main/java/com/github/charlyb01/sihywtcamc/mixin/sweep/PlayerSweepAttackMixin.java
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/sweep/PlayerSweepAttackMixin.java
@@ -1,22 +1,16 @@
 package com.github.charlyb01.sihywtcamc.mixin.sweep;
 
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerSweepAttackMixin extends LivingEntity {
-    protected PlayerSweepAttackMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
-
-    @ModifyVariable(method = "attack", name = "bl4", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/entity/LivingEntity;)I"))
+public class PlayerSweepAttackMixin {
+    @ModifyVariable(method = "attack", ordinal = 3, at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/entity/LivingEntity;)I"))
     private boolean sweepingIfEnchant(boolean bl4) {
-        return bl4 && (EnchantmentHelper.getSweepingMultiplier(this) > 0);
+        return bl4 && (EnchantmentHelper.getSweepingMultiplier((LivingEntity) (Object) this) > 0);
     }
 }
