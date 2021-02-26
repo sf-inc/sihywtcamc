84204d9608f49de85051bfefd25427bad00b0880
Charly <charly5bollinger@gmail.com>
2021-02-26 18:12:17 +0100

    Refactor cancel eat code
    I coded this with way too hard things while it was just easy. No need a variable to detect a damage because there is a damage method call when damaged

--- src/main/java/com/github/charlyb01/sihywtcamc/imixin/IPlayerDamagedMixin.java
+++
@@ -1,5 +0,0 @@
-package com.github.charlyb01.sihywtcamc.imixin;
-
-public interface IPlayerDamagedMixin {
-    boolean getDamaged();
-}
---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/drink/PlayerEatDrinkMixin.java
@@ -0,0 +1,29 @@
+package com.github.charlyb01.sihywtcamc.mixin.drink;
+
+import com.github.charlyb01.sihywtcamc.config.ModConfig;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.PotionItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEatDrinkMixin extends LivingEntity {
+    protected PlayerEatDrinkMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;dropShoulderEntities()V"))
+    private void cancelEatDrink(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (ModConfig.get().generalConfig.eatDrinkCancellable) {
+            if (this.getActiveItem().isFood() || this.getActiveItem().getItem() instanceof PotionItem) {
+                this.clearActiveItem();
+            }
+        }
+    }
+}
