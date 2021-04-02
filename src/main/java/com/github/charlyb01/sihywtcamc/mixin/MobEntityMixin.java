02fcc2ae214b67326bc76e1c355df04776e77bf6
Charly <charly5bollinger@gmail.com>
2021-04-02 17:19:56 +0200

    Flint and steel sets entities on fire
    For 3s, while it's for 8s when they cross fire on ground

---
+++ src/main/java/com/github/charlyb01/sihywtcamc/mixin/MobEntityMixin.java
@@ -0,0 +1,46 @@
+package com.github.charlyb01.sihywtcamc.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+    @Shadow public abstract void setTarget(@Nullable LivingEntity target);
+
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
+    private void setOnFireMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.getItem().equals(Items.FLINT_AND_STEEL)) {
+            this.world.playSound(player, this.getX(), this.getY(), this.getZ(), SoundEvents.ITEM_FLINTANDSTEEL_USE, this.getSoundCategory(), 1.0F, this.random.nextFloat() * 0.4F + 0.8F);
+            if (!this.world.isClient) {
+                this.setOnFireFor(3);
+                if (!player.isCreative()) {
+                    this.setTarget(player);
+                }
+                itemStack.damage(1, player, playerEntity -> playerEntity.sendEquipmentBreakStatus(hand.equals(Hand.MAIN_HAND) ?
+                        EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND));
+            }
+
+            cir.setReturnValue(ActionResult.success(this.world.isClient));
+        }
+    }
+}
