b3d28409e9d8180c4068d672f2450bad4c0d911e
charlyb01 <charly5bollinger@gmail.com>
2024-11-06 21:34:24 +0100

    Add mod id utils

--- src/main/java/com/github/charlyb01/sihywtcamc/Sihywtcamc.java
+++ src/main/java/com/github/charlyb01/sihywtcamc/Sihywtcamc.java
@@ -8,8 +8,11 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class Sihywtcamc implements ModInitializer {
+    public static final String MOD_ID = "sihywtcamc";
+
     @Override
     public void onInitialize() {
         AutoConfig.register(ModConfig.class, PartitioningSerializer.wrap(GsonConfigSerializer::new));
@@ -17,5 +20,9 @@ public class Sihywtcamc implements ModInitializer {
         AttributeModifierCallback.init();
         MaxStackSizeCallback.init();
         EnchantmentModifyCallback.init();
+    }
+
+    public static Identifier id(final String path) {
+        return Identifier.of(MOD_ID, path);
     }
 }
